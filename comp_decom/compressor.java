adaaaa0d43ab5240b01d25df6532a901fb88855f
Rajat <rs.sunny100@gmail.com>
2023-05-14 01:34:31 +0530

    first commit

---
+++ comp_decom/compressor.java
@@ -0,0 +1,33 @@
+package comp_decom;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+public class compressor {
+    public static void method(File file) throws IOException{
+
+        String fileDirectory = file.getParent();
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(fileDirectory + "/compressed.gz");
+        GZIPOutputStream gzip = new GZIPOutputStream(fos);
+
+        byte[] buffer = new byte[1024];
+        int len;
+
+        while((len = fis.read(buffer)) != -1){
+            gzip.write(buffer,0,len);
+
+        }
+        gzip.close();
+        fos.close();
+        fis.close();
+    }
+
+    public static void main(String [] args) throws IOException{
+        File path = new File("C:/Users/rssun/OneDrive/Desktop/rajat/compress1.txt");
+        method(path);
+    }
+}
